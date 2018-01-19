eb14ef0e8cae22d260358561f8d59cf8f5f1a7a5
Kelly Grenz <kelly@witcreative-studio.com>
2018-01-19 09:53:39 -0700

    logo added

--- client/src/components/structure/header/Header.js
+++ client/src/components/structure/header/Header.js
@@ -9,12 +9,17 @@ const style = {
     
     background: '#607D8B',
     justifyContent: 'space-between'
+  },
+  headerImg: {
+    width: '206px',
+    height: '35px',
+    padding: '25px'
   }
 }
 
 const Header = ({ domainData }) =>
   <div style={style.container}>
-    <img src="..../images/logo.png" style={style.headerImg}/>
+    <img src="images/logo_sm.png" style={style.headerImg}/>
     <NavItem exact to='/' style={style.link}> Home </NavItem>
     <NavItem to='/about' style={style.link}> About </NavItem>
     <NavItem to='/products' style={style.link}> Products </NavItem>
